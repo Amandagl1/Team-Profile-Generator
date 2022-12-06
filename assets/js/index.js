8154aa7a9dcc10546299edd547f47e73bc5f50cc
Amanda Lee <leeandama87@gmail.com>
2022-12-06 00:17:18 -0800

    Inserted index.html into index.js using back ticks.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,29 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHtml = ({ employee, id, email, officeNumber })
+const generateHTML = ({ employee, id, email, officeNumber }) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>README Generator</title>
+</head>
+<body>
+    <div class="card border-info mb-3" style="max-width: 18rem;">
+        <div class="card-header"></div>
+        <div class="card-body text-info">
+          <h4 class="card-title">${employee}</h4>
+          <p class="card-text">ID: ${id}</p>
+          <p class="card-text">Office Number: ${officeNumber}</p>
+        </div>
+      </div>
+</body>
+</html>`
 
 inquirer
+// User prompt to create index.html
 .prompt([
     {
     // User selects if they are an Manager, Engineer, or Intern
