ee96f433826b13925deca0d67404d3f930a0e9f3
Amanda Lee <leeandama87@gmail.com>
2022-12-08 21:56:40 -0800

    Made changes to index.js. Created an if statement for the each position in the team.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const express = require('express');
 const app = express();
+
+const fs = require('fs');
+
+const express = require('express');
 const generateHTML = ({ employee, name, email, id, github }) => 
 `<!DOCTYPE html>
 <html lang="en">
@@ -39,7 +41,8 @@ const generateHTML = ({ employee, name, email, id, github }) =>
 </html>`
 
 // User prompt to create index.html
-inquirer.prompt([
+inquirer
+  .prompt([
     {
     // User selects if they are an Manager, Engineer, or Intern
       type: 'list',
@@ -51,47 +54,70 @@ inquirer.prompt([
       ],
       name: 'employee'
     },
+
     {
       type: 'input',
       message: 'What is their name?',
       name: 'name',
     },
+
     {
       type: 'input',
       message: 'What is their email?',
       name: 'email',
     },
+
     {
       type: 'input',
       message: 'What is their ID?',
       name: 'id',
     },
+
     {
       type: 'input',
       message: 'What is your GitHub username?',
       name: 'github',
     },
-    {
-      type: 'choices',
-      message: 'Any other team members?',
-      choices: [
-        'Yes',
-        'No'
-      ],
-      name: 'members',
-
-    },
     
 
   ])
 
-  .then((answers) => {
+  .then((position) => {
+    let teamMember = '';
+    if (position === 'Manager') {
+      teamMember = 'Office Number';
+    } else if (position === 'Engineer') {
+      teamMember = 'GitHub Username';
+    } else {
+      position = 'School Name';
+    };
+  
+  });
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Any other team members?',
+        choices: [
+          'Add an engineer',
+          'Add an intern',
+          'Finish building team'
+        ],
+        name: 'members',
+  
+      },
+    ])
+
+      .then((answers) => {
     const htmlPageContent = generateHTML(answers);
     fs.writeFile('index.html', htmlPageContent, (err) =>
     err ? console.log(err) : console.log('Successfully created index.html!')
+  
+    )},
   );
-  }
-  );
+
+ 
 
 app.use(express.static(__dirname + 'assets/css/style.css'));
 
